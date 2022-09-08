3edfb426865ebad23dee771a97455694b1e22371
lilianguima <lilianm.guima@gmail.com>
2022-09-08 17:30:26 -0300

    Exercício 3

---
+++ README.md
@@ -0,0 +1,7 @@
+# Variables--javaScript
+nome - string
+sobrenome - string
+peso - number
+altura - number
+dataDeInternacao - number 
+diabético - boolean
---
+++ Variables-javaScript.js
@@ -0,0 +1,7 @@
+//peso, nome, altura, data de internação, dadosSe é diabético ou não
+const prompt = require ('prompt-sync')()
+const Peso = prompt ('Seu Peso: ');
+const Name = prompt ('Seu Nome: ');
+const Altura = prompt ('Sua Altura: ');
+const dataDeInternacao = prompt ('Data de Internação: ');
+const seDiabetico = prompt ('É diabetico? ');
